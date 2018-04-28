69534dcc0f3a5043000dbcd49516161447c7d98e
imwaykul <imwaykul@gmail.com>
2018-04-27 20:13:24 -0400

    Add files via upload

--- guess_the_mood.py
+++ guess_the_mood.py
@@ -11,8 +11,8 @@ from collections import defaultdict
 #n-gram
 def mood_to_phrase_2gram(file):
     mood_to_sent_count = defaultdict(float)
-    most_common_words_happy = defaultdict(lambda:1.6)
-    most_common_words_sad = defaultdict(lambda:1.6)
+    most_common_words_happy = defaultdict(lambda:4.2)
+    most_common_words_sad = defaultdict(lambda:4.2)
     openFile = open(file, "r", encoding="utf-8")
     readlines = openFile.readlines()
     print(len(readlines))
@@ -33,12 +33,13 @@ def mood_to_phrase_2gram(file):
                 most_common_words_sad[sentence[index].lower()] += 1
             mood_to_sent_count[bigram] += 1
             index = index + 1
+    openFile.close()
     return mood_to_sent_count, most_common_words_happy, most_common_words_sad
             
 def mood_to_phrase_3gram(file):
     mood_to_sent_count = defaultdict(float)
-    most_common_words_happy = defaultdict(lambda:1.6)
-    most_common_words_sad = defaultdict(lambda:1.6)
+    most_common_words_happy = defaultdict(lambda:0.5)
+    most_common_words_sad = defaultdict(lambda:0.5)
     openFile = open(file, "r", encoding="utf-8")
     readlines = openFile.readlines()
     print(len(readlines))
@@ -57,8 +58,9 @@ def mood_to_phrase_3gram(file):
                 most_common_words_happy[sentence[index].lower()] += 1
             if (mood.lower() == "sad" and sentence[index] != "SENT-HEAD"):
                 most_common_words_sad[sentence[index].lower()] += 1
-            mood_to_sent_count[bigram] += 1
+            mood_to_sent_count[trigram] += 1
             index = index + 1
+    openFile.close()
     return mood_to_sent_count, most_common_words_happy, most_common_words_sad
 
 def normalize_dict2(m_to_s):
@@ -85,7 +87,7 @@ def normalize_dict3(m_to_s):
         #print("CURRLEN: m_to_s.items: ", len(m_to_s.items()))
         opp = getOpposite(key[0])
         newTrigram = (opp, key[1], key[2], key[3])
-        nVal = copy_dict[newBigram]
+        nVal = copy_dict[newTrigram]
         total = nVal + value
         norm_dict[key] = value/total
         if (nVal == 0):
@@ -121,11 +123,12 @@ def paragraphAnalyzer(sentences, factor):
 #st = st.strip("\n")
 #print(st)
 
-datum, mcwh, mcws = mood_to_phrase_2gram("sentient_data.txt")
-#datum, mcwh, mcws = mood_to_phrase_3gram("sentient_data.txt")
-norm_dict = normalize_dict2(datum)
+datum2, mcwh2, mcws2 = mood_to_phrase_2gram("sentient_data.txt")
+norm_dict2 = normalize_dict2(datum2)
+datum3, mcwh3, mcws3 = mood_to_phrase_3gram("sentient_data.txt")
+norm_dict3 = normalize_dict3(datum3)
 
-def testing(sentencez):
+def testing2(sentencez, detailed):
     count = 0
     correct = []
     wrong = []
@@ -147,16 +150,16 @@ def testing(sentencez):
             hap_bigram = ("HAPPY", sentence[index].lower(), sentence[index+1].lower())
             #print(hap_bigram)
             sad_bigram = ("SAD", sentence[index].lower(), sentence[index+1].lower())
-            hapScor = norm_dict[hap_bigram]
-            sScor = norm_dict[sad_bigram]
+            hapScor = norm_dict2[hap_bigram]
+            sScor = norm_dict2[sad_bigram]
             if (hapScor == 0):
                 firstWord = sentence[index].lower()
                 secondWord = sentence[index+1].lower()
                 #thirdWord = sentence[index+2].lower()
-                happy_index = mcwh[firstWord] + mcwh[secondWord]
-                sad_index = mcws[firstWord] + mcwh[secondWord]
-                #happy_index = mcwh[firstWord] + mcwh[secondWord]
-                #sad_index = mcws[firstWord] + mcwh[secondWord]
+                happy_index = mcwh2[firstWord] + mcwh2[secondWord]
+                sad_index = mcws2[firstWord] + mcws2[secondWord]
+                #happy_index = mcwh[firstWord] + mcwh[secondWord] + mcwh[thirdWord]
+                #sad_index = mcws[firstWord] + mcws[secondWord] + mcws[thirdWord]
                 hapScor = happy_index/ (happy_index + sad_index)
                 sadScor = sad_index/ (sad_index+ happy_index)
                 #hapScor = 0.5
@@ -181,12 +184,135 @@ def testing(sentencez):
             wrong.append(sent[1])
     accuracy = float(len(correct))/float(len(sentencez))
     print("You got ", (accuracy * 100), "% correct")
-    print("Incorrect Sentences: ")
-    for wr in wrong:
-        print("----")
-        print(wr)
+    if (detailed == True):
+        print("Incorrect Sentences: ")
+        for wr in wrong:
+            print("----")
+            print(wr)
+
+def testing3(sentencez, detailed):
+    count = 0
+    correct = []
+    wrong = []
+    #print(s
+    for sent in sentencez:
+        #sent = input("Welcome! Enter a sentence and we'll tell you the mood \n")
+        sentence = sent[1].split(" ")
+        #print(sent[0])
+        #print(sentence)
+        index = 0
+        #PREPROCESS
+        sentence.insert(0,"SENT-HEAD")
+        sentence[len(sentence)-1] = sentence[len(sentence)-1].strip("\n")
+        sentence.append("SENT-TAIL")
+        happyScore = 100.00
+        sadScore = 100.00
+        cap = 1
+        while (sentence[index+cap] != "SENT-TAIL"):
+            hap_bigram = ("HAPPY", sentence[index].lower(), sentence[index+1].lower(), sentence[index+2].lower())
+            #print(hap_bigram)
+            sad_bigram = ("SAD", sentence[index].lower(), sentence[index+1].lower(), sentence[index+2].lower())
+            hapScor = norm_dict3[hap_bigram]
+            sScor = norm_dict3[sad_bigram]
+            if (hapScor == 0):
+                firstWord = sentence[index].lower()
+                secondWord = sentence[index+1].lower()
+                thirdWord = sentence[index+2].lower()
+#                happy_index = mcwh[firstWord] + mcwh[secondWord]
+#                sad_index = mcws[firstWord] + mcws[secondWord]
+                happy_index = mcwh3[firstWord] + mcwh3[secondWord] + mcwh3[thirdWord]
+                sad_index = mcws3[firstWord] + mcws3[secondWord] + mcws3[thirdWord]
+                hapScor = happy_index/ (happy_index + sad_index)
+                sadScor = sad_index/ (sad_index+ happy_index)
+                #hapScor = 0.5
+                #sadScor = 0.5
+            happyScore = happyScore * hapScor
+            sadScore = sadScore * sadScor
+            index = index + 1
+        #print("happy score: ", happyScore)
+        #print("sad score: ", sadScore)
+        verdict = ""
+        if (happyScore > sadScore):
+            #print("You're in a Happy Mood")
+            verdict = "happy"
+        elif (happyScore == sadScore):
+            verdict = "neutral"
+        else:
+            #print("Why the sad face?")
+            verdict = "sad"
+        if ((sent[0].lower() == "happy" and verdict == "happy") or (sent[0].lower() == "sad" and verdict == "sad")):
+            correct.append(sent[1])
+        else:
+            wrong.append(sent[1])
+    accuracy = float(len(correct))/float(len(sentencez))
+    print("You got ", (accuracy * 100), "% correct")
+    if (detailed == True):
+        print("Incorrect Sentences: ")
+        for wr in wrong:
+            print("----")
+            print(wr)
+        
+def add2Data(fileName, sentence, mood):
+    file = open(fileName, "a")
+    newsent = mood +": "  + sentence + "\n"
+    file.write(newsent)
+    file.close()
                            
-            
+def trySent():
+    count = 0
+    correct = []
+    wrong = []
+    while (input("Would you like to continue \n ?") == "y"):
+        sent = input("Welcome! Enter a sentence and we'll tell you the mood \n")
+        actualMood = input("What's the mood?: ")
+        if (actualMood.lower() == "h" or actualMood.lower() == "happy"):           
+            add2Data("sentient_data.txt", sent, "Happy")
+        else:
+            add2Data("sentient_data.txt", sent, "Sad")
+        sentence = sent[1].split(" ")
+        #print(sent[0])
+        #print(sentence)
+        index = 0
+        #PREPROCESS
+        sentence.insert(0,"SENT-HEAD")
+        sentence[len(sentence)-1] = sentence[len(sentence)-1].strip("\n")
+        sentence.append("SENT-TAIL")
+        happyScore = 100.00
+        sadScore = 100.00
+        cap = 0
+        while (sentence[index+cap] != "SENT-TAIL"):
+            #hap_bigram = ("HAPPY", sentence[index].lower(), sentence[index+1].lower(), sentence[index+2].lower())
+            #print(hap_bigram)
+            hap_bigram = ("HAPPY", sentence[index].lower(), sentence[index+1].lower())
+            #sad_bigram = ("SAD", sentence[index].lower(), sentence[index+1].lower(), sentence[index+2].lower())
+            sad_bigram = ("SAD", sentence[index].lower(), sentence[index+1].lower())
+
+            hapScor = norm_dict[hap_bigram]
+            sScor = norm_dict[sad_bigram]
+            if (hapScor == 0):
+                firstWord = sentence[index].lower()
+                secondWord = sentence[index+1].lower()
+                #thirdWord = sentence[index+2].lower()
+                happy_index = mcwh[firstWord] + mcwh[secondWord]
+                sad_index = mcws[firstWord] + mcws[secondWord]
+ #               happy_index = mcwh[firstWord] + mcwh[secondWord] + mcwh[thirdWord]
+#                sad_index = mcws[firstWord] + mcws[secondWord] + mcws[thirdWord]
+                hapScor = happy_index/ (happy_index + sad_index)
+                sadScor = sad_index/ (sad_index+ happy_index)
+                #hapScor = 0.5
+                #sadScor = 0.5
+            happyScore = happyScore * hapScor
+            sadScore = sadScore * sadScor
+            index = index + 1
+        #print("happy score: ", happyScore)
+        #print("sad score: ", sadScore)
+        verdict = ""
+        if (happyScore > sadScore):
+            #print("You're in a Happy Mood")
+            verdict = "happy"
+        else:
+            #print("Why the sad face?")
+            verdict = "sad"          
             
         
     
@@ -200,11 +326,17 @@ sampleSentences = [("Happy", "I feel so perfect today"), ("Happy", "The sun is s
                    ,("Happy", "Life has been great to me"), ("Sad", "I want to kill myself"),
                    ("Happy", "Please take care of yourself"), ("Sad", "That test went bad"),
                    ("Happy", "I am living a great lifestyle"), ("Sad", "I dread my existence"), ("Sad", "Is there a point in living"),
-                   ("Happy", "I can't wait to see the movie"), ("Happy", "This is going to be so much fun!"), ("Happy", "I want to see you be happy")]
+                   ("Happy", "I can't wait to see the movie"), ("Happy", "This is going to be so much fun!"), ("Happy", "I want to see you be happy"), ("Happy", "I cannot wait for tomorrow"),
+                   ("Happy", "The world is filled with opportunities"), ("Sad", "They never got a fair share in life"),
+                   ("Sad", "I think we should break up now"), ("Happy", "Be happy with what you have") ]
                    
 
+print("BIGRAM RESULTS: ")
+testing2(sampleSentences, False)
+print("...............")
 
-testing(sampleSentences)
+print("TRIGRAM RESULTS: ")
+testing3(sampleSentences, False)
 
     
     
